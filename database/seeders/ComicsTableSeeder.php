21119d74f92b99fc587dbb80b7dc2a1ac7ba4e07
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-16 18:19:37 +0100

    Populated table

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +15,20 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            $new_comics = new Comic();
+            $new_comics->title = $comic['title'];
+            $new_comics->description = $comic['description'];
+            $new_comics->thumb = $comic['thumb'];
+            $new_comics->price = $comic['price'];
+            $new_comics->series = $comic['series'];
+            $new_comics->sale_date = $comic['sale_date'];
+            $new_comics->type = $comic['type'];
+            $new_comics->artists = implode(', ', $comic['artists']);
+            $new_comics->writers = implode(', ', $comic['writers']);
+            $new_comics->save();
+        }
     }
 }
