9e14e75be5274fe275b166c15653038883ff177b
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-19 15:06:43 +0100

    Added header and footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,68 @@
+<footer>
+
+    {{-- TOP FOOTER --}}
+    <div class="top-footer">
+        <div class="container">
+            <div class="footer-logo-dc d-flex">
+                <div class="menu">
+                    <h3 class="text-uppercase">DC Comics</h3>
+                    <ul class="list-unstyled">
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Collectibles</li>
+                        <li>Videos</li>
+                        <li>Fans</li>
+                        <li>News</li>
+                        <li>Shop</li>
+                    </ul>
+                    <h3 class="text-uppercase mt-3">shop</h3>
+                    <ul class="list-unstyled">
+                        <li>Shop DC</li>
+                        <li>Shop DC collectibles</li>
+                    </ul>
+                </div>
+                <div class="menu ms-5">
+                    <h3 class="text-uppercase">dc</h3>
+                    <ul class="list-unstyled">
+                        <li>Terms Of Use</li>
+                        <li>Privacy Police</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscription</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="menu ms-5">
+                    <h3 class="text-uppercase">sites</h3>
+                    <ul class="list-unstyled">
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- BOTTOM FOOTER --}}
+    <div class="bottom-footer">
+        <div class="container">
+            <div class="row">
+                <div class="d-flex justify-content-between align-items-center">
+                    <button>sign-up now!</button>
+                    <a href="#">follow us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,30 @@
+<header>
+    <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-2">
+                <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="" class="img-fluid">
+            </div>
+            <div class="col-8">
+                <ul class="list-unstyled d-flex justify-content-center">
+                    <li>
+                        <a href="{{ route('homepage') }}">home</a>
+                    </li>
+                    <li><a href="">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">shop
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-2"></div>
+        </div>
+    </div>
+</header>
